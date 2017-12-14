703b13d1ef34edf062cdc2634f5dcd356635e17a
dariasteam <dariasteam94@gmail.com>
2017-12-14 10:58:30 +0000

    Refactorizado

--- dna.cpp
+++ dna.cpp
@@ -1,3 +1,14 @@
 #include "dna.h"
 
+dna::~dna() {
+  free (sequence);
+}
 
+void dna::operator=(const dna &aux) {
+  unsigned sz;
+  input_neurons = aux.input_neurons;
+  output_neurons = aux.output_neurons;
+  memcpy(&sz, aux.sequence, sizeof(sz));
+  sequence = (char*)malloc(sz + sizeof(sz));
+  memcpy(sequence, aux.sequence, sz + sizeof(sz));
+}
--- dna.h
+++ dna.h
@@ -10,17 +10,8 @@ struct dna {
   unsigned input_neurons;
   unsigned output_neurons;
 
-  ~dna () {
-    free (sequence);
-  }
-  void operator= (const dna& aux) {
-    unsigned sz;
-    input_neurons = aux.input_neurons;
-    output_neurons = aux.output_neurons;
-    memcpy(&sz, aux.sequence, sizeof(sz));
-    sequence = (char*)malloc(sz + sizeof(sz));
-    memcpy(sequence, aux.sequence, sz + sizeof(sz));
-  }
+  ~dna ();
+  void operator= (const dna& aux);
 };
 
 #endif // DNA_H
